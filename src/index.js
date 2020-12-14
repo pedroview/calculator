4a5fd8bff690a3db8c8417fed968011dcfb85aa1
pedromass <maduekwepedro@gmail.com>
2020-12-15 00:27:54 +0100

    re-arranged import statement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Material UI
-import { MuiThemeProvider } from "@material-ui/core";
 // component entry and mui theme config
 import Calculator from "./component/calculator";
 import theme from "./source/theme";
+
+// Material UI
+import { MuiThemeProvider } from "@material-ui/core";
+
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
